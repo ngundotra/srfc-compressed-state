786a3bb0bd437040bf232f077e6907dc74e20843
ngundotra <noah@gundotra.org>
2023-05-31 15:21:02 -0400

    generic compression

--- programs/compressed-state/src/lib.rs
+++ programs/compressed-state/src/lib.rs
@@ -1,10 +1,75 @@
 use anchor_lang::prelude::*;
+use anchor_lang::Discriminator;
 use spl_account_compression;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// pub struct ConcurrentMerkleTree<const MAX_DEPTH: usize, const MAX_BUFFER_SIZE: usize> {
+//     pub sequence_number: u64,
+//     /// Index of most recent root & changes
+//     pub active_index: u64,
+//     /// Number of active changes we are tracking
+//     pub buffer_size: u64,
+//     /// Proof for respective root
+//     pub change_logs: [ChangeLog<MAX_DEPTH>; MAX_BUFFER_SIZE],
+//     pub rightmost_proof: Path<MAX_DEPTH>,
+// }
+
+// pub struct ChangeLog<const MAX_DEPTH: usize> {
+//     /// Historical root value before Path was applied
+//     pub root: Node,
+//     /// Nodes of off-chain merkle tree
+//     pub path: [Node; MAX_DEPTH],
+//     /// Bitmap of node parity (used when hashing)
+//     pub index: u32,
+//     pub _padding: u32,
+// }
+
+// #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+// #[repr(C)]
+// pub struct Path<const MAX_DEPTH: usize> {
+//     pub proof: [Node; MAX_DEPTH],
+//     pub leaf: Node,
+//     pub index: u32,
+//     pub _padding: u32,
+// }
+
+fn get_rightmost_proof_index(
+    tree_bytes: &[u8],
+    max_depth: u32,
+    max_buffer_size: u32,
+) -> Result<u32> {
+    let changelog_size = 32 + 32 * max_depth + 4 + 4;
+    let path_start = 8 + 8 + 8 + changelog_size * max_buffer_size;
+
+    let index_start = path_start + 32 * max_depth + 32;
+    let (_, _index_bytes) = tree_bytes.split_at(index_start as usize);
+    let (index_bytes, _) = _index_bytes.split_at(4);
+
+    let index = u32::try_from_slice(index_bytes)?;
+    Ok(index)
+}
+
+fn get_rightmost_proof_index_from_account<'info>(account: &AccountInfo<'info>) -> Result<u32> {
+    let merkle_tree_bytes = account.try_borrow_data()?;
+    let (header_bytes, rest) = merkle_tree_bytes
+        .split_at(spl_account_compression::state::CONCURRENT_MERKLE_TREE_HEADER_SIZE_V1);
+    let header =
+        spl_account_compression::state::ConcurrentMerkleTreeHeader::try_from_slice(header_bytes)?;
+    let merkle_tree_size = spl_account_compression::state::merkle_tree_get_size(&header)?;
+    let (tree_bytes, _canopy_bytes) = rest.split_at(merkle_tree_size);
+    let rmp_index = get_rightmost_proof_index(
+        tree_bytes,
+        header.get_max_depth(),
+        header.get_max_buffer_size(),
+    )?;
+    Ok(rmp_index)
+}
+
 #[program]
 pub mod compressed_state {
+    use spl_account_compression::merkle_tree_apply_fn;
+
     use super::*;
 
     pub fn init_new_tree(
@@ -17,6 +82,7 @@ pub mod compressed_state {
             b"__event_authority".as_ref(),
             &[*ctx.bumps.get("event_authority").unwrap()],
         ];
+
         let authority_pda_signer = &[&seeds[..]];
         let cpi_ctx = CpiContext::new_with_signer(
             ctx.accounts.spl_ac.to_account_info(),
@@ -36,10 +102,26 @@ pub mod compressed_state {
             asset_id: ctx.accounts.tree.key.clone(),
             owner: ctx.accounts.owner.key.clone(),
         };
-        let bytes = new_state.try_to_vec().unwrap();
+
+        let rmp_index = get_rightmost_proof_index_from_account(&ctx.accounts.tree)?;
+        msg!("Rmp: {}", rmp_index);
+
+        // Asset ID is a PDA of tree & leaf index it is being minted to
+        let asset_id = Pubkey::find_program_address(
+            &[ctx.accounts.tree.key.as_ref(), &rmp_index.to_le_bytes()],
+            &crate::ID,
+        )
+        .0;
+
+        let mut bytes = State::DISCRIMINATOR.try_to_vec()?;
+        let state_bytes = new_state.try_to_vec().unwrap();
+        bytes.extend_from_slice(&state_bytes.clone());
         emit_cpi!({
-            CrudCreateBytes {
-                payload: bytes.clone(),
+            CrudCreate {
+                owner: ctx.accounts.owner.key.clone(),
+                asset_id: asset_id,
+                pubkeys: vec![],
+                data: bytes.clone(),
             }
         });
 
@@ -58,7 +140,8 @@ pub mod compressed_state {
             },
             authority_pda_signer,
         );
-        let leaf: [u8; 32] = anchor_lang::solana_program::hash::hashv(&[&bytes]).to_bytes();
+        let leaf: [u8; 32] = anchor_lang::solana_program::keccak::hashv(&[&state_bytes]).to_bytes();
+        msg!("Leaf: {:?}", leaf);
         spl_account_compression::cpi::append(cpi_ctx, leaf)?;
         Ok(())
     }
@@ -70,16 +153,20 @@ pub mod compressed_state {
         root: Pubkey,
         leaf_index: u32,
     ) -> Result<()> {
-        if *ctx.accounts.owner.key != new_state.owner {
+        if *ctx.accounts.owner.key != old_state.owner {
             return Err(ErrorCode::InvalidOwner.into());
         }
+
+        let rmp_index = get_rightmost_proof_index_from_account(&ctx.accounts.tree)?;
+        msg!("Rmp: {}", rmp_index);
 
         let previous_bytes = old_state.try_to_vec().unwrap();
         let new_bytes = new_state.try_to_vec().unwrap();
 
         emit_cpi!({
             CrudUpdateBytes {
-                payload: new_bytes.clone(),
+                asset_id: old_state.asset_id,
+                data: new_bytes.clone(),
             }
         });
 
@@ -99,8 +186,9 @@ pub mod compressed_state {
             authority_pda_signer,
         );
         let previous_leaf: [u8; 32] =
-            anchor_lang::solana_program::hash::hashv(&[&previous_bytes]).to_bytes();
-        let new_leaf: [u8; 32] = anchor_lang::solana_program::hash::hashv(&[&new_bytes]).to_bytes();
+            anchor_lang::solana_program::keccak::hashv(&[&previous_bytes]).to_bytes();
+        let new_leaf: [u8; 32] =
+            anchor_lang::solana_program::keccak::hashv(&[&new_bytes]).to_bytes();
         spl_account_compression::cpi::replace_leaf(
             cpi_ctx,
             root.to_bytes(),
@@ -163,53 +251,15 @@ pub struct State {
 }
 
 #[event]
-pub struct CrudCreateBytes {
-    pub payload: Vec<u8>,
+pub struct CrudCreate {
+    pub owner: Pubkey,
+    pub asset_id: Pubkey,
+    pub pubkeys: Vec<Pubkey>,
+    pub data: Vec<u8>,
 }
 
 #[event]
 pub struct CrudUpdateBytes {
-    pub payload: Vec<u8>,
+    pub asset_id: Pubkey,
+    pub data: Vec<u8>,
 }
-
-// pub struct CrudtCreate<P: AccountSerialize + AccountDeserialize + Sized> {
-//     payload: P,
-// }
-// impl<P> borsh::ser::BorshSerialize for CrudtCreate<P>
-// where
-//     P: borsh::ser::BorshSerialize,
-// {
-//     fn serialize<W: borsh::maybestd::io::Write>(
-//         &self,
-//         writer: &mut W,
-//     ) -> ::core::result::Result<(), borsh::maybestd::io::Error> {
-//         borsh::BorshSerialize::serialize(&self.payload, writer)?;
-//         Ok(())
-//     }
-// }
-// impl<P> borsh::de::BorshDeserialize for CrudtCreate<P>
-// where
-//     P: borsh::BorshDeserialize,
-// {
-//     fn deserialize(buf: &mut &[u8]) -> ::core::result::Result<Self, borsh::maybestd::io::Error> {
-//         Ok(Self {
-//             payload: borsh::BorshDeserialize::deserialize(buf)?,
-//         })
-//     }
-// }
-// impl<P> anchor_lang::Event for CrudtCreate<P>
-// where
-//     P: AccountSerialize + AccountDeserialize + Sized,
-// {
-//     fn data(&self) -> Vec<u8> {
-//         let mut d = [158, 23, 22, 123, 251, 96, 205, 150].to_vec();
-//         d.append(&mut self.try_to_vec().unwrap());
-//         d
-//     }
-// }
-// impl<P> anchor_lang::Discriminator for CrudtCreate<P>
-// where
-//     P: AccountSerialize + AccountDeserialize + Sized,
-// {
-//     const DISCRIMINATOR: [u8; 8] = [158, 23, 22, 123, 251, 96, 205, 150];
-// }
--- tests/compressed-state.ts
+++ tests/compressed-state.ts
@@ -3,12 +3,165 @@ import { Program } from "@coral-xyz/anchor";
 import { CompressedState } from "../target/types/compressed_state";
 
 import {
-  DepthSizePair,
   ValidDepthSizePair,
   SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
   SPL_NOOP_PROGRAM_ID,
   createAllocTreeIx,
+  MerkleTree,
+  ConcurrentMerkleTreeAccount,
+  SPL_NOOP_ADDRESS,
+  SPL_ACCOUNT_COMPRESSION_ADDRESS,
 } from "@solana/spl-account-compression";
+import { keccak_256 } from "js-sha3";
+import { Keypair, PublicKey } from "@solana/web3.js";
+
+type UniversalIx = {
+  programId: anchor.web3.PublicKey;
+  keys: anchor.web3.PublicKey[];
+  data: Buffer;
+};
+
+function orderInstructions(tx: anchor.web3.TransactionResponse): UniversalIx[] {
+  let accounts: anchor.web3.PublicKey[] =
+    tx.transaction.message.staticAccountKeys;
+  accounts = accounts.concat(tx.meta.loadedAddresses.writable ?? []);
+  accounts = accounts.concat(tx.meta.loadedAddresses.readonly ?? []);
+
+  let ordered: UniversalIx[] = [];
+
+  let outerIdx = 0;
+  let innerIdx = 0;
+  for (const outerIxFlat of tx.transaction.message.instructions) {
+    let outerIx: UniversalIx = {
+      programId: accounts[outerIxFlat.programIdIndex],
+      keys: outerIxFlat.accounts.map((idx) => accounts[idx]),
+      data: anchor.utils.bytes.bs58.decode(outerIxFlat.data),
+    };
+    ordered.push(outerIx);
+
+    const innerIxBucket = tx.meta?.innerInstructions[innerIdx];
+    if (innerIxBucket && innerIxBucket.index === outerIdx) {
+      for (const innerIxFlat of innerIxBucket.instructions) {
+        let innerIx: UniversalIx = {
+          programId: accounts[innerIxFlat.programIdIndex],
+          keys: innerIxFlat.accounts.map((idx) => accounts[idx]),
+          data: anchor.utils.bytes.bs58.decode(innerIxFlat.data),
+        };
+        ordered.push(innerIx);
+      }
+      innerIdx += 1;
+    }
+    outerIdx += 1;
+  }
+  return ordered;
+}
+
+const CPI_EVENT_IX: Buffer = Buffer.from([
+  0xe4, 0x45, 0xa5, 0x2e, 0x51, 0xcb, 0x9a, 0x1d,
+]);
+
+// Parses CPI events from a transaction for the given anchor program
+function parseCpiEvents(
+  tx: anchor.web3.TransactionResponse,
+  program: anchor.Program
+): anchor.Event[] {
+  let orderedIxs = orderInstructions(tx);
+  // console.log(orderedIxs);
+
+  let events: anchor.Event[] = [];
+  for (let i = 1; i < orderedIxs.length; i += 1) {
+    const ix = orderedIxs[i];
+
+    if (ix.data.slice(0, 8).equals(CPI_EVENT_IX)) {
+      const eventAuthority = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("__event_authority")],
+        program.programId
+      )[0];
+
+      // CHECK that the event authority is the CPI authority
+      if (
+        ix.keys.length != 1 ||
+        ix.keys[0].toBase58() !== eventAuthority.toBase58()
+      ) {
+        continue;
+      }
+
+      const eventData = anchor.utils.bytes.base64.encode(ix.data.slice(8));
+      const event = program.coder.events.decode(eventData);
+      events.push(event);
+    }
+  }
+
+  return events;
+}
+
+type CrudCreate = {
+  assetId: PublicKey;
+  owner: PublicKey;
+  pubkeys: PublicKey[];
+  data: Buffer;
+};
+
+type State = {
+  assetId: PublicKey;
+  owner: PublicKey;
+};
+
+function deserializeCrudCreate(
+  program: anchor.Program<CompressedState>,
+  event: CrudCreate
+): State {
+  let data = program.coder.accounts.decode("State", event.data);
+  return data as State;
+}
+
+class CompressedStateIndexer<S> {
+  programId: anchor.web3.PublicKey;
+  treeId: anchor.web3.PublicKey;
+  tree: MerkleTree;
+  compressedState: Map<String, S>;
+
+  constructor(
+    programId: anchor.web3.PublicKey,
+    treeId: anchor.web3.PublicKey,
+    config: ValidDepthSizePair
+  ) {
+    this.programId = programId;
+    this.treeId = treeId;
+    this.tree = MerkleTree.sparseMerkleTreeFromLeaves([], config.maxDepth);
+    this.compressedState = new Map<String, S>();
+  }
+
+  createNewState(index: number, state: S, serializedBytes: Buffer) {
+    let assetId = this.getAssetId(index);
+
+    let leaf = Buffer.from(keccak_256.digest(serializedBytes));
+    this.tree.updateLeaf(index, leaf);
+    this.compressedState.set(assetId.toBase58(), state);
+  }
+
+  getAssetId(index: number) {
+    return anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        this.treeId.toBuffer(),
+        Buffer.from(new anchor.BN(index).toArray("le", 8)),
+      ],
+      this.programId
+    )[0];
+  }
+
+  getState(assetId: PublicKey): S {
+    return this.compressedState.get(assetId.toBase58());
+  }
+
+  getStateByIndex(index: number): S {
+    return this.getState(this.getAssetId(index));
+  }
+
+  getProof(index: number) {
+    return this.tree.getProof(index);
+  }
+}
 
 describe("compressed-state", () => {
   // Configure the client to use the local cluster.
@@ -22,6 +175,12 @@ describe("compressed-state", () => {
     maxDepth: 14,
     maxBufferSize: 64,
   };
+
+  let compressedIndexer = new CompressedStateIndexer(
+    program.programId,
+    merkleTree,
+    merkleTreeConfig
+  );
 
   it("Initialize tree", async () => {
     // Add your test here.
@@ -46,8 +205,6 @@ describe("compressed-state", () => {
     console.log("Your transaction signature", tx);
   });
   it("Create new state", async () => {
-    console.log("Neehaw");
-
     const tx = await program.methods
       .createNewState()
       .accounts({
@@ -59,18 +216,69 @@ describe("compressed-state", () => {
       .rpc({ skipPreflight: true, commitment: "confirmed" });
 
     console.log("Your transaction signature", tx);
+
+    let cpiEvents = parseCpiEvents(
+      await program.provider.connection.getTransaction(tx, {
+        commitment: "confirmed",
+      }),
+      program
+    );
+
+    let deserializedEvent = deserializeCrudCreate(
+      program,
+      cpiEvents[0].data as CrudCreate
+    );
+
+    compressedIndexer.createNewState(
+      0,
+      deserializedEvent,
+      (cpiEvents[0].data.data as Buffer).slice(8)
+    );
+    console.log("State:", deserializedEvent);
   });
   it("Update state", async () => {
+    let index = 0;
+    let proof = compressedIndexer.getProof(index);
+
+    let cmt = await ConcurrentMerkleTreeAccount.fromAccountAddress(
+      program.provider.connection,
+      merkleTree,
+      "confirmed"
+    );
+    let proofVerified = MerkleTree.verify(cmt.getCurrentRoot(), proof);
+    if (!proofVerified) {
+      console.log("Verification result:", proofVerified);
+      console.log("Root:", cmt.getCurrentRoot());
+      console.log("Root:", proof.root);
+      throw new Error("Proof invalid");
+    }
+    let oldAsset = compressedIndexer.getStateByIndex(0);
     const tx = await program.methods
-      .createNewState()
+      .replace(
+        {
+          owner: Keypair.generate().publicKey,
+          assetId: compressedIndexer.getAssetId(index),
+        },
+        oldAsset,
+        new PublicKey(proof.root),
+        index
+      )
       .accounts({
         owner: program.provider.publicKey,
         tree: merkleTree,
         splAc: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
         noop: SPL_NOOP_PROGRAM_ID,
       })
+      .remainingAccounts(
+        proof.proof.map((p) => {
+          return {
+            isSigner: false,
+            isWritable: false,
+            pubkey: new PublicKey(p),
+          };
+        })
+      )
       .rpc({ skipPreflight: true, commitment: "confirmed" });
-
-    console.log("Teehaw");
+    console.log("Your transaction signature", tx);
   });
 });
